058111167a83067af7dd4798e08ac9af1387cc25
Kyle <kyle.c.r.fahey@gmail.com>
2017-08-01 11:11:32 -0500

    Front page done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
-import TiSocialFacebookCircular from 'react-icons/lib/ti/social-facebook-circular';
 import FacebookLogin from 'react-facebook-login'
 
-import { connect } from 'react-redux'
+import {connect} from 'react-redux'
 import {setFaceBookToken, setTwitterUsername} from './actions/action'
 
 
@@ -14,38 +13,72 @@ import {setFaceBookToken, setTwitterUsername} from './actions/action'
   }
 })
 export class App extends Component {
- 
+  constructor() { 
+  super()
+
+    this.state = {
+      twittername: ''
+    }
+
+  }
   render() {
     const responseFacebook = (response) => {
       console.log(response);
       this.props.dispatch(setFaceBookToken(response.accessToken))
     }
 
-    const handleSubmit = () => {
-      console.log(this.props.value)
-      this.props.dispatch(setTwitterUsername(this.props.value))
+    const handleSubmit = (e) => {
+      this.props.dispatch(setTwitterUsername(e.target.value))
     }
 
     return (
       <div className="App">
-         <FacebookLogin
-            appId="1345711915541627"
-            autoLoad={true}
-            fields="name,email,picture"
-            scope="user_posts,public_profile"
-            callback={responseFacebook}
-            icon={<TiSocialFacebookCircular />}
-          />
+         
+         <div className="login">
 
-          <form onSubmit={this.handleSubmit}>
-        <label>
-          Twitter Username
-          <input type="text" value={this.props.value}  />
-           </label>
-            <input type="submit" value="Submit" />
-          </form>
-   
+           <br />
+           <br />
 
+            <h1 className="repressed-title">Personality Insights by:</h1>
+            <h3 className="repressed-title">@kylecrfahey</h3>
+
+
+            <br /> 
+            <br /> 
+            <br />
+            <br />
+
+
+            <div className="inline-icons">
+              <FacebookLogin
+                  appId="1345711915541627"
+                  autoLoad={true}
+                  fields="name,email,picture"
+                  scope="user_posts,public_profile"
+                  callback={responseFacebook}
+                />
+              <i className="fa fa-check" aria-hidden="true"></i>
+            </div>
+              
+
+            <div className="inline">
+              <span>
+                <div className="form-container">
+                  <input className="form-text" type="text" name="twitter" placeholder="Twitter Username" onChange={handleSubmit} pattern="^[A-Za-z0-9_]{1,32}$" required/>
+                </div>
+                </span>
+                <i className="fa fa-check twitter" aria-hidden="true"></i>
+            </div>
+
+                <div>
+
+                  <button className="start-button">Start The Adventure</button> 
+                  <i className="fa fa-arrow-left" aria-hidden="true"></i>
+                </div>
+              </div>
+
+
+          
       </div>
     );
   }
