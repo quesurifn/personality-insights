c5f3361825af99e561f8a53aa2a4d24e55f6eaec
Kyle <kyle.c.r.fahey@gmail.com>
2017-08-01 20:15:58 -0500

    Props Fixed; Validation;

--- src/App.js
+++ src/App.js
@@ -7,28 +7,36 @@ import {setFaceBookToken, setTwitterUsername} from './actions/action'
 
 
 @connect((store) => {
+  console.log('store',store)
   return {
-    fbkey: store.fbkey,
-    twittername: store.twittername
+    fbkey: store.reducer.fbkey,
+    twittername: store.reducer.twittername
   }
 })
 export class App extends Component {
-  constructor() { 
-  super()
 
-    this.state = {
-      twittername: ''
-    }
-
-  }
   render() {
     const responseFacebook = (response) => {
       console.log(response);
+      let facebook = document.querySelector('.facebook')
+      facebook.style.display = 'inline'
       this.props.dispatch(setFaceBookToken(response.accessToken))
     }
 
     const handleSubmit = (e) => {
+      let arrow =  document.querySelector('.next')
+      let twitter = document.querySelector('.twitter')
+      if (e.target.checkValidity()) {
+      
+      twitter.style.display = 'inline'
       this.props.dispatch(setTwitterUsername(e.target.value))
+      //console.log(store.getState())
+      document.querySelector('.next').style.display = "inline"
+    } else {
+
+        arrow.style.display = 'none'
+        twitter.style.display = 'none'
+      }
     }
 
     return (
@@ -57,7 +65,7 @@ export class App extends Component {
                   scope="user_posts,public_profile"
                   callback={responseFacebook}
                 />
-              <i className="fa fa-check" aria-hidden="true"></i>
+              <i className="fa fa-check facebook" aria-hidden="true"></i>
             </div>
               
 
@@ -71,15 +79,11 @@ export class App extends Component {
             </div>
 
                 <div>
-
                   <button className="start-button">Start The Adventure</button> 
-                  <i className="fa fa-arrow-left" aria-hidden="true"></i>
+                  <i className="fa fa-arrow-left next" aria-hidden="true"></i>
                 </div>
-              </div>
-
-
-          
-      </div>
+              </div>  
+        </div>
     );
   }
 }
